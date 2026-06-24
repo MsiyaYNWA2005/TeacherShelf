d0a7dc80f9542f021c23f1482800f84ede9e1516
Msiya2841214 <2841214@students.wits.ac.za>
2026-06-25 01:18:00 +0200

    Moderation  cards-count

--- src/pages/Moderation.jsx
+++ src/pages/Moderation.jsx
@@ -1,9 +1,76 @@
 import "../cssfiles/Moderation.css"
 import { User } from "lucide-react";
+import { db } from "../firebaseConnSetUp";
+import { collection,getDocs,query,where} from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 
 
 function Moderation(){
+
+
+    const [pendingCount,setpendingCount] = useState(0);
+    const [approvedCount,setapprovedCount] = useState(0);
+    const [rejectedCount,setrejectedCount] = useState(0);
+
+
+
+
+    //i useEffect becuase i want this to run when moderations runs
+    useEffect ( function(){
+        async function GetBooksPending(){
+
+            const q = query(collection(db,"books"),
+                    where("status" ,"==","pending")
+
+                );
+            const snapshots = await getDocs(q);
+            setpendingCount(snapshots.size);
+        }
+        GetBooksPending()
+    },[]);
+
+
+
+    useEffect ( function(){
+        async function GetBooksApproved(){
+
+        const q = query(collection(db,"books"),
+                where("status" ,"==","pending")
+
+            );
+        const snapshots = await getDocs(q);
+        setapprovedCount(snapshots.size);
+
+        }
+        GetBooksApproved()
+    },[]);
+
+
+
+
+
+
+    useEffect ( () =>{
+        async function GetBooksRejected(){
+
+            const q = query(collection(db,"books"),
+                    where("status" ,"==","rejected")
+
+                );
+            const snapshots = await getDocs(q);
+            setrejectedCount(snapshots.size);
+
+        }
+        GetBooksRejected()
+    },[]);
+
+
+
+
+
+
+
     return (
        <section className="Moderation-main-section">
        
@@ -27,20 +94,20 @@ function Moderation(){
             <section className="pending-card">
 
               <section className="pending-h2-p">
-                <h2>N0.</h2>
+                <h2>{pendingCount}</h2>
                 <p>Pending</p>
                  </section>
                
             </section>
             <section className="approved-card">
                <section className="approved-h2-p">
-                 <h2>N0.</h2>
+                 <h2>{approvedCount}</h2>
                 <p>Approved</p>
                </section>
             </section>
             <section className="rejected-card">
                 <section className="rejected-h2-p">
-                    <h2>N0.</h2>
+                    <h2>{rejectedCount}</h2>
                     <p>Rejected</p>
                 </section>
             </section>
