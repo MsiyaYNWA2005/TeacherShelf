2a569400f6e35673f4f43ee91ff47cda54a611ff
Msiya2841214 <2841214@students.wits.ac.za>
2026-06-26 14:40:31 +0200

    Moderation  approve,pending,rejected contents

--- src/pages/Moderation.jsx
+++ src/pages/Moderation.jsx
@@ -2,7 +2,7 @@ import "../cssfiles/Moderation.css"
 import { User } from "lucide-react";
 import { db } from "../firebaseConnSetUp";
 import { collection,getDocs,query,where} from "firebase/firestore";
-import { useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import { Check } from "lucide-react";
 
 
@@ -10,27 +10,22 @@ import { Check } from "lucide-react";
 function Moderation(){
 
 
-    const [pendingCount,setpendingCount] = useState(0);
-    const [approvedCount,setapprovedCount] = useState(0);
-    const [rejectedCount,setrejectedCount] = useState(0);
+    const [pendingCount,setPendingCount] = useState(0);
+    const [approvedCount,setApprovedCount] = useState(0);
+    const [rejectedCount,setRejectedCount] = useState(0);
+    const [PendingBooks,setPendingBooks]   = useState([]);
+    const [RejectedBooks,setRejectedBooks]   = useState([]);
+    const [ApprovedBooks,setApprovedBooks]   = useState([]);
+    const [selectedBookState,setSelectedBookState] = useState(null);
+
+
+    
 
 
 
 
     //i useEffect becuase i want this to run when moderations runs
-    useEffect ( function(){
-        async function GetBooksPending(){
-
-            const q = query(collection(db,"books"),
-                    where("status","==","pending")
-
-                );
-            const snapshots = await getDocs(q);
-            setpendingCount(snapshots.size);
-        }
-        GetBooksPending()
-    },[]);
-
+    
 
 
     useEffect ( function(){
@@ -41,11 +36,91 @@ function Moderation(){
 
             );
         const snapshots = await getDocs(q);
-        setapprovedCount(snapshots.size);
+          const books=[];
+
+
+            for(let i =0;i<snapshots.size;i++){
+                const data = snapshots.docs[i].data();
+
+                books.push({
+                    author:data.author,
+                    book_title:data.bookTitle,
+                    grade:data.gradeLevel,
+                    subject :data.subject,
+                    reason_recommend:data.reason,
+                    photo_Url:data.photoURL,
+                    Date : data.createdAt
+
+                })
+            }
+
+        setApprovedBooks(books);
+        setApprovedCount(snapshots.size);
 
         }
         GetBooksApproved()
     },[]);
+
+
+
+    
+
+
+    function display_approved_book(){
+      if(ApprovedBooks.length == 0){
+           return(
+                <section className="section-no-books">
+                <section className="books-check">
+                    <Check size={20} color="#8B5A0E" />
+                </section>
+                <section className="book-headers">
+                    <h2>Nothing here</h2>
+                    <p>No approved submissions yet.</p>
+                </section>
+            </section>
+           );
+         }
+            const approved = [];
+            for(let i =0;i<ApprovedBooks.length;i++){
+                const array_books=ApprovedBooks[i];
+                approved.push(
+                    <section className="books-section"   key={i}>
+                    <section className="subject-grade-date-section">
+                    <section>
+                        <p>{array_books.Date}</p>
+                    </section>
+
+                    <section>
+                        <p>{array_books.subject}</p>
+                        <p>{array_books.grade}</p>
+                    </section>
+                </section>
+                <section className="book_title-author-reason">
+                    <h2>{array_books.book_title}</h2>
+                    <p>{array_books.author}</p>
+                    <p>{array_books.reason_recommend}</p>
+                </section>
+
+                <section className="book-photo">
+                    <img 
+                    src={array_books.photo_Url} 
+                    alt="Description of the image" 
+                    style={{ width: '350px', height: 'auto' }} 
+                    />
+                </section>
+
+                <section className="buttons-approve-reject">
+                    <button className="approve-button">Approve</button>
+                    <button className="reject-button">Rejected</button>
+                </section>
+                </section>
+                )
+                
+            
+         }
+
+         return approved;
+    }
 
 
 
@@ -59,27 +134,16 @@ function Moderation(){
                     where("status" ,"==","rejected")
 
                 );
-            const snapshots = await getDocs(q);
-            setrejectedCount(snapshots.size);
+           const snapshots = await getDocs(q);
+           
 
-        }
-        GetBooksRejected()
-    },[]);
-
-    async function GetPendingBooks(){
-            const q = query(collection(db,"books"),
-                    where("status","==","pending")
-
-                );
-            const snapshots = await getDocs(q);
-
-            const books_pending=[];
+            const books=[];
 
 
             for(let i =0;i<snapshots.size;i++){
                 const data = snapshots.docs[i].data();
 
-                books_pending.push({
+                books.push({
                     author:data.author,
                     book_title:data.bookTitle,
                     grade:data.gradeLevel,
@@ -91,47 +155,56 @@ function Moderation(){
                 })
             }
 
-            return books_pending;
+            setRejectedBooks(books);
+            setRejectedCount(snapshots.size);
 
-    }
+        }
+        GetBooksRejected()
+    },[]);
 
-    async function  display_pending_book(){
 
-         const array_pending= await GetPendingBooks();
 
-         if(array_pending.length == 0){
-            <section className="section_pending_no_books">
-                <section className="pending-check">
+
+
+
+    function display_rejected_books(){
+         if(RejectedBooks.length == 0){
+           return(
+                <section className="section-no-books">
+                <section className="books-check">
                     <Check size={20} color="#8B5A0E" />
                 </section>
-                <section className="pending-headers">
+                <section className="book-headers">
                     <h2>Nothing here</h2>
-                    <p>No rejected submissions yet.</p>
+                    <p>No approved submissions yet.</p>
                 </section>
             </section>
+           );
          }
-         else{
-            for(let i =0;i<array_pending.length;i++){
-                <section className="pending-books-section">
+            const rejected = [];
+            for(let i =0;i<RejectedBooks.length;i++){
+                const array_books=RejectedBooks[i];
+                rejected.push(
+                    <section className="books-section"   key={i}>
                     <section className="subject-grade-date-section">
                     <section>
-                        <p>{array_pending[i].Date}</p>
+                        <p>{array_books.Date}</p>
                     </section>
 
                     <section>
-                        <p>{array_pending[i].subject}</p>
-                        <p>{array_pending[i].grade}</p>
+                        <p>{array_books.subject}</p>
+                        <p>{array_books.grade}</p>
                     </section>
                 </section>
                 <section className="book_title-author-reason">
-                    <h2>{array_pending[i].book_title}</h2>
-                    <p>{array_pending[i].author}</p>
-                    <p>{array_pending[i].reason_recommend}</p>
+                    <h2>{array_books.book_title}</h2>
+                    <p>{array_books.author}</p>
+                    <p>{array_books.reason_recommend}</p>
                 </section>
 
                 <section className="book-photo">
                     <img 
-                    src={array_pending[i].photo_Url} 
+                    src={array_books.photo_Url} 
                     alt="Description of the image" 
                     style={{ width: '350px', height: 'auto' }} 
                     />
@@ -142,15 +215,123 @@ function Moderation(){
                     <button className="reject-button">Rejected</button>
                 </section>
                 </section>
+                )
                 
-            }
+            
          }
+
+         return rejected;
     }
 
 
 
+    
+    useEffect ( function(){
+     async function GetPendingBooks(){
+            const q = query(collection(db,"books"),
+                    where("status","==","pending")
+
+                );
+            const snapshots = await getDocs(q);
+           
+
+            const books=[];
 
 
+            for(let i =0;i<snapshots.size;i++){
+                const data = snapshots.docs[i].data();
+
+                books.push({
+                    author:data.author,
+                    book_title:data.bookTitle,
+                    grade:data.gradeLevel,
+                    subject :data.subject,
+                    reason_recommend:data.reason,
+                    photo_Url:data.photoURL,
+                    Date : data.createdAt
+
+                })
+            }
+
+            setPendingBooks(books);
+            setPendingCount(snapshots.size);
+
+    }
+     GetPendingBooks();
+    },[])
+
+
+
+   
+function  display_pending_books(){
+
+    
+         if(PendingBooks.length == 0){
+           return(
+                <section className="section-no-books">
+                <section className="books-check">
+                    <Check size={20} color="#8B5A0E" />
+                </section>
+                <section className="book-headers">
+                    <h2>Nothing here</h2>
+                    <p>No pending submissions yet.</p>
+                </section>
+            </section>
+           );
+         }
+            const pending = [];
+            for(let i =0;i<PendingBooks.length;i++){
+                const array_books=PendingBooks[i];
+                pending.push(
+                    <section className="books-section"   key={i}>
+                    <section className="subject-grade-date-section">
+                    <section>
+                        <p>{array_books.Date}</p>
+                    </section>
+
+                    <section>
+                        <p>{array_books.subject}</p>
+                        <p>{array_books.grade}</p>
+                    </section>
+                </section>
+                <section className="book_title-author-reason">
+                    <h2>{array_books.book_title}</h2>
+                    <p>{array_books.author}</p>
+                    <p>{array_books.reason_recommend}</p>
+                </section>
+
+                <section className="book-photo">
+                    <img 
+                    src={array_books.photo_Url} 
+                    alt="Description of the image" 
+                    style={{ width: '350px', height: 'auto' }} 
+                    />
+                </section>
+
+                <section className="buttons-approve-reject">
+                    <button className="approve-button">Approve</button>
+                    <button className="reject-button">Rejected</button>
+                </section>
+                </section>
+                )
+                
+            
+         }
+
+         return pending;
+    }
+
+    function display_Active_Button_Books(){
+        if(selectedBookState === "pending"){
+            return display_pending_books();
+        }
+        if(selectedBookState === "approved"){
+            return display_approved_book();
+        }
+        if(selectedBookState === "rejected"){
+            return display_rejected_books();
+        }
+    }
 
 
     return (
@@ -198,21 +379,23 @@ function Moderation(){
         <section className="state-section">
 
           <section className="pending-">
-            <button onClick={display_pending_book} className="pending-button">Pending</button>
+            <button onClick={ function() {setSelectedBookState("pending");}} className="pending-button">Pending</button>
             <span>({pendingCount})</span>
           </section>
 
           <section className="approved-">
-            <button className="pending-button">Approved</button>
+            <button onClick={ function() {setSelectedBookState("approved");}} className="pending-button">Approved</button>
             <span>({approvedCount})</span>
           </section>
 
           <section className="rejected-">
-            <button className="pending-button">Rejected</button>
+            <button  onClick={ function() {setSelectedBookState("rejected");}} className="pending-button">Rejected</button>
             <span>({rejectedCount})</span>
           </section>
 
         </section>
+
+        {display_Active_Button_Books()}
 
        </section>
     )
